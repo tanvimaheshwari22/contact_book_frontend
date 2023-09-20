93d6d29c878f5a91d045465275414b37bd5939ae
Tanvi <tanvi.maheshwari@argusoft.com>
2023-09-20 10:15:19 +0530

    add root files for ui src

---
+++ ui/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      react app
+    </div>
+  );
+}
+
+export default App;
---
+++ ui/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from './components/login/LoginPage';
+import PageNotFound from './pages/notFound';
+import UnAuthorizedPage from './pages/unauthorized';
+import Registration from './pages/registration';
+import Dashboard from './pages/dashboard';
+import MainPage from './pages/main';
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+          <Route path="/dash"   element={<Dashboard />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/Registration" element={<Registration />} />
+        <Route path="/unauthorized" element={<UnAuthorizedPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
